26b80c6c32f31b667a7994670efdf36fd2952ffa
Ngoc Sy <bearof1999@gmail.com>
2021-03-26 23:21:56 +0700

    fix database

--- ecommerce.sql
+++ ecommerce.sql
@@ -1,26 +1,25 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
--- http://www.phpmyadmin.net
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2021 at 04:52 AM
--- Server version: 5.5.32
--- PHP Version: 5.4.16
+-- Generation Time: Mar 26, 2021 at 05:21 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `ecommerce`
 --
-CREATE DATABASE IF NOT EXISTS `ecommerce` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `ecommerce`;
 
 -- --------------------------------------------------------
 
@@ -28,12 +27,11 @@ USE `ecommerce`;
 -- Table structure for table `category`
 --
 
-CREATE TABLE IF NOT EXISTS `category` (
-  `CateID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `category` (
+  `CateID` int(11) NOT NULL,
   `CateName` varchar(150) NOT NULL,
-  `CateDescription` varchar(500) NOT NULL,
-  PRIMARY KEY (`CateID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+  `CateDescription` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `category`
@@ -50,12 +48,10 @@ INSERT INTO `category` (`CateID`, `CateName`, `CateDescription`) VALUES
 -- Table structure for table `orderdetail`
 --
 
-CREATE TABLE IF NOT EXISTS `orderdetail` (
+CREATE TABLE `orderdetail` (
   `Quantity` int(11) NOT NULL,
   `OrderID` int(11) NOT NULL,
-  `ProductID` int(11) NOT NULL,
-  PRIMARY KEY (`OrderID`,`ProductID`),
-  KEY `Quantity` (`Quantity`)
+  `ProductID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -64,14 +60,13 @@ CREATE TABLE IF NOT EXISTS `orderdetail` (
 -- Table structure for table `orderproduct`
 --
 
-CREATE TABLE IF NOT EXISTS `orderproduct` (
-  `OrderID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `orderproduct` (
+  `OrderID` int(11) NOT NULL,
   `OrderDate` datetime NOT NULL,
   `ShipDate` datetime NOT NULL,
   `ShipName` varchar(150) NOT NULL,
-  `ShipAddress` varchar(200) NOT NULL,
-  PRIMARY KEY (`OrderID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `ShipAddress` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -79,23 +74,94 @@ CREATE TABLE IF NOT EXISTS `orderproduct` (
 -- Table structure for table `product`
 --
 
-CREATE TABLE IF NOT EXISTS `product` (
-  `ProductID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `product` (
+  `ProductID` int(11) NOT NULL,
   `ProductName` varchar(150) NOT NULL,
   `CateID` int(11) NOT NULL,
   `PriceProduct` double NOT NULL,
   `Quantity` int(11) NOT NULL,
   `Description` varchar(500) NOT NULL,
-  `Picture` varchar(200) NOT NULL,
-  PRIMARY KEY (`ProductID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+  `Picture` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `product`
 --
 
 INSERT INTO `product` (`ProductID`, `ProductName`, `CateID`, `PriceProduct`, `Quantity`, `Description`, `Picture`) VALUES
-(2, 'Nokia 2', 1, 300000, 5, 'Mô tả Nokia', '/public/imageProduct/1.jpg');
+(5, 'Nokia 2', 1, 300000, 1, '1', '20210326044124Screenshot (1).png'),
+(6, 'Sp1', 1, 4, 4, 'hi', '20210326051740Screenshot (1).png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`CateID`);
+
+--
+-- Indexes for table `orderdetail`
+--
+ALTER TABLE `orderdetail`
+  ADD PRIMARY KEY (`OrderID`,`ProductID`),
+  ADD KEY `Quantity` (`Quantity`),
+  ADD KEY `ProductID` (`ProductID`);
+
+--
+-- Indexes for table `orderproduct`
+--
+ALTER TABLE `orderproduct`
+  ADD PRIMARY KEY (`OrderID`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`ProductID`),
+  ADD KEY `CateID` (`CateID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `CateID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `orderproduct`
+--
+ALTER TABLE `orderproduct`
+  MODIFY `OrderID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orderdetail`
+--
+ALTER TABLE `orderdetail`
+  ADD CONSTRAINT `orderdetail_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `orderproduct` (`OrderID`),
+  ADD CONSTRAINT `orderdetail_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ProductID`);
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`CateID`) REFERENCES `category` (`CateID`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
