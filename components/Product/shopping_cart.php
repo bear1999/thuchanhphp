4797a49086aef17d0c4098922856aa21968eed92
sinhvien <sinhvien>
2021-03-30 10:07:07 +0700

    up lai

--- components/Product/shopping_cart.php
+++ components/Product/shopping_cart.php
@@ -15,20 +15,21 @@ if(isset($_GET["id"])) {
     if(!isset($_SESSION["cart_items"]) || count($_SESSION["cart_items"]) < 1) {
         $_SESSION["cart_items"] = array(0 => array("pro_id" => $pro_id, "quantity" => 1));
     }
-} else {
-    foreach($_SESSION["cart_items"] as $item) {
-        $i++;
-        while(list($key, $value) = each($item)) {
-            if($key == "pro_id" && $value == $pro_id) {
-                array_splice($_SESSION["cart_tem"], $i-1, 1, array(array("pro_id" => $pro_id, "quantity" => $item["quantity"] + 1)));
-                $was_found = true;
+    else {
+        foreach($_SESSION["cart_items"] as $item) {
+            $i++;
+            while(list($key, $value) = each($item)) {
+                if($key == "pro_id" && $value == $pro_id) {
+                    array_splice($_SESSION["cart_items"], $i-1, 1, array(array("pro_id" => $pro_id, "quantity" => $item["quantity"] + 1)));
+                    $was_found = true;
+                }
             }
         }
+        if($was_found == false) {
+            array_push($_SESSION["cart_items"], array("pro_id" => $pro_id, "quanity" => 1));
+        }
+        header("Location: ./shopping_cart.php");
     }
-    if($was_found == false) {
-        array_push($_SESSION["cart_items"], array("pro_id" => $pro_id, "quanity" => 1));
-    }
-    header("Location: ./shopping_cart.php");
 }
 ?>
 
@@ -67,7 +68,7 @@ if(isset($_GET["id"])) {
                             echo "
                                 <tr>
                                     <td>".$prod["ProductName"]."</td>
-                                    <td><img style='width: 90px; height: 80px;' src='../../public/assets/imageProduct/".$prod["Picture"]."'></td>
+                                    <td><img style='width: 90px; height: 80px;' src='../../public/imageProduct/".$prod["Picture"]."'></td>
                                     <td>".$item["quantity"]."</td>
                                     <td>".$prod["PriceProduct"]."</td>
                                     <td>".$prod["PriceProduct"]."</td>
